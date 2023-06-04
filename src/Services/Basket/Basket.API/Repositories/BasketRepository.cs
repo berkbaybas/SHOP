c8ce1a6840273f65552820d323bd40b4e776bc85
berk <berk_baybas@hotmail.com>
2023-06-04 23:31:52 +0300

    implement Basket.API

---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,36 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCache;
+        public BasketRepository(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache;
+        }
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            string basket = await _redisCache.GetStringAsync(userName);
+            if (string.IsNullOrEmpty(basket)) 
+                return null;
+
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+            
+        }
+
+        public Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            _redisCache.SetStringAsync(basket.UserName, JsonConvert.SerializeObject(basket));
+            return GetBasket(basket.UserName);
+        }
+
+        public async Task DeleteBasket(string userName)
+        {
+            await _redisCache.RemoveAsync(userName);
+        }
+    }
+}

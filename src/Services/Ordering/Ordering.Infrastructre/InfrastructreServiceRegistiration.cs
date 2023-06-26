20e5b058ef1957ae524849d04bf45b62673162dd
berk <berk_baybas@hotmail.com>
2023-06-26 15:18:59 +0300

    dockerize Discount.Grpc implement Ordering.API with DDD, CQRS, Onion Architecture dockerize Ordering.API

---
+++ src/Services/Ordering/Ordering.Infrastructre/InfrastructreServiceRegistiration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Models;
+using Ordering.Infrastructre.Mail;
+using Ordering.Infrastructre.Persistence;
+using Ordering.Infrastructre.Repository;
+
+namespace Ordering.Infrastructre
+{
+    public static class InfrastructreServiceRegistiration
+    {
+        public static IServiceCollection AddInfrastructreServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<OrderContext>(options =>
+              options.UseSqlServer(configuration.GetConnectionString("OrderingConnectionString")));
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+            services.AddScoped<IOrderRepository, OrderRepository>();
+
+            services.Configure<EmailSettings>(c => configuration.GetSection("EmailSettings"));
+            services.AddTransient<IEmailService, EmailService>();
+
+            return services;
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructre/Persistence/OrderContextSeed.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructre.Persistence
+{
+    public sealed class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfiguredOrders());
+
+                await orderContext.SaveChangesAsync();
+
+                logger.LogInformation("Seed database associated with context {DbContextName}", typeof(OrderContext).Name);
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order() {
+                    UserName = "swn", 
+                    FirstName = "Berk", 
+                    LastName = "Baybas", 
+                    EmailAddress = "berk@berk.berk", 
+                    AddressLine = "Nilüfer", 
+                    Country = "Turkey", 
+                    TotalPrice = 350 }
+            };
+        }
+    }
+}
